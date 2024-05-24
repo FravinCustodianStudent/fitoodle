0b51f4a997cad545496d9fdafcf3cc075b8e7300
Fravin <3114226@knu.ua>
2024-05-24 14:22:35 +0300

    testPage.js

--- src/components/tests/descItem/descrItem.js
+++ src/components/tests/descItem/descrItem.js
@@ -1,7 +1,7 @@
 import "./descrItem.scss";
 const DescrItem = ({number,content,answerType}) =>{
     return(
-        <div className={answerType ? "question__main__info__content__descr__item answerDescr" : "question__main__info__content__descr__item"}>
+        <div className={answerType ? "question__main__info__content__descr__item" : "question__main__info__content__descr__item  answerDescr"}>
             <div className="question__main__info__content__descr__item__number">
                 {number}
             </div>
--- src/components/tests/testPage.js
+++ src/components/tests/testPage.js
@@ -4,7 +4,6 @@ import "./testPage.scss";
 import QuestionItem from "./questionItem/questionItem";
 import questionSrc from "../../assets/question.svg";
 import DescrItem from "./descItem/descrItem";
-import boldArrowSrc from "../../assets/boldArrow.svg";
 import arrowtSrc from "../../assets/arrow.svg";
 
 const TestPage = () =>{
@@ -45,127 +44,34 @@ const TestPage = () =>{
                                 Питання 2 - співставте відповіді
                             </div>
                         </div>
-
+                        <div className="question__main__info__header__description">
+                            Lorem ipsum dolor sit amet consectetur. Vel odio dui in pellentesque commodo urna. Urna tristique sit eu magna mi integer vitae dictum. Ultrices sodales amet pharetra lectus at urna. Enim ut semper ut rhoncus. In accumsan.
+                        </div>
 
                     </div>
                     <div className="question__main__info__content">
                         <div className="question__main__info__content__descr">
-                            <DescrItem number={1}
+                            <DescrItem number={'A'}
                                        content={"Lorem ipsum dolor sit amet consectetur. Eget diam habitasse hac nam pulvinar massa velit. "} />
-                            <DescrItem number={2}
+                            <DescrItem number={'B'}
                                        content={"Lorem ipsum dolor sit amet consectetur. Eget diam habitasse hac nam pulvinar massa velit. "} />
-                            <DescrItem number={3}
-                                       content={"Lorem ipsum dolor sit amet consectetur. Eget diam habitasse hac nam pulvinar massa velit. "} />
-                            <DescrItem number={4}
-                                       content={"Lorem ipsum dolor sit amet consectetur. Consectetur interdum morbi nisi proin ac nam consectetur quis ultrices. Eu tellus proin enim accumsan eget arcu. Amet porta ornare arcu lacus ornare. Neque nibh commodo nulla ultricies neque augue. In ut ornare sapien metus ac eget nec. "} />
                         </div>
                         <div className="question__main__info__content__descr">
-                            <DescrItem number={'A'} answerType={true}
-                                       content={"Lorem ipsum dolor sit amet consectetur. Eget diam habitasse hac nam pulvinar massa velit. "} />
-                            <DescrItem number={'B'} answerType={true}
-                                       content={"Lorem ipsum dolor sit amet consectetur. Eget diam habitasse hac nam pulvinar massa velit. "} />
                             <DescrItem number={'C'} answerType={true}
                                        content={"Lorem ipsum dolor sit amet consectetur. Eget diam habitasse hac nam pulvinar massa velit. "} />
-                            <DescrItem number={'D'} answerType={true}
-                                       content={"Lorem ipsum dolor sit amet consectetur. Consectetur interdum morbi nisi proin ac nam consectetur quis ultrices. Eu tellus proin enim accumsan eget arcu. Amet porta ornare arcu lacus ornare. Neque nibh commodo nulla ultricies neque augue. In ut ornare sapien metus ac eget nec. "} />
-                        </div>
-                    </div>
-
-                </div>
-                <div className="question__main__answer">
-                    <div className="question__main__answer__content">
-                        <div className="question__main__answer__content__header">
-                            <div className="question__main__answer__content__header__item">
-                                <HandySvg src={boldArrowSrc}/>
-                            </div>
-                            <div className="question__main__answer__content__header__item">
-                                A
-                            </div>
-                            <div className="question__main__answer__content__header__item">
-                                B
-                            </div>
-                            <div className="question__main__answer__content__header__item">
-                                C
-                            </div>
-                            <div className="question__main__answer__content__header__item">
-                                D
-                            </div>
-                        </div>
-                        <div className="question__main__answer__content__row">
-                            <div className="question__main__answer__content__row__number">
-                                    1
-                            </div>
-                            <div className="question__main__answer__content__row__item">
-
-                            </div>
-                            <div className="question__main__answer__content__row__item">
-
-                            </div>
-                            <div className="question__main__answer__content__row__item selected__answer">
-
-                            </div>
-                            <div className="question__main__answer__content__row__item">
-
-                            </div>
-                        </div><div className="question__main__answer__content__row">
-                        <div className="question__main__answer__content__row__number">
-                            2
-                        </div>
-                        <div className="question__main__answer__content__row__item">
-
-                        </div>
-                        <div className="question__main__answer__content__row__item">
-
-                        </div>
-                        <div className="question__main__answer__content__row__item ">
-
-                        </div>
-                        <div className="question__main__answer__content__row__item">
+                            <DescrItem number={'D'}
+                                       content={"Lorem ipsum dolor sit amet consectetur. Eget diam habitasse hac nam pulvinar massa velit. "} />
 
                         </div>
                     </div>
-                        <div className="question__main__answer__content__row">
-                            <div className="question__main__answer__content__row__number">
-                                3
-                            </div>
-                            <div className="question__main__answer__content__row__item">
-
-                            </div>
-                            <div className="question__main__answer__content__row__item">
-
-                            </div>
-                            <div className="question__main__answer__content__row__item ">
-
-                            </div>
-                            <div className="question__main__answer__content__row__item">
-
-                            </div>
-                        </div>
-                        <div className="question__main__answer__content__row">
-                            <div className="question__main__answer__content__row__number">
-                                4
-                            </div>
-                            <div className="question__main__answer__content__row__item">
-
-                            </div>
-                            <div className="question__main__answer__content__row__item">
-
-                            </div>
-                            <div className="question__main__answer__content__row__item">
-
-                            </div>
-                            <div className="question__main__answer__content__row__item">
-
-                            </div>
-                        </div>
-
-
-                    </div>
-                    <div className="question__main__answer__container">
-                        <div className="question__main__answer__container__button">
-                            <HandySvg src={arrowtSrc}/>
-                        </div>
-                    </div>
+                     <div className="question__main__info__container">
+                         <div className="question__main__info__container__button-text">
+                             Закінчити роботу над тестом
+                         </div>
+                         <div className="question__main__info__container__button">
+                             Наступне питання <HandySvg src={arrowtSrc}/>
+                         </div>
+                     </div>
                 </div>
             </div>
         </div>
